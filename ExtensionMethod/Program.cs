67347231aa1fe172fd5c17411f311c19ba9b2887
haybe <haybe@EWDOD>
2023-08-23 11:20:26 +0300

    Add project files.

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ExtensionMethod;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(7.Karesi());
+
+        Console.WriteLine(3.Uzeri(4));
+    }
+
+}
+
+public static class Sayi
+{
+    public static int Karesi(this int sayi)
+    {
+        return sayi * sayi;
+    }
+    public static int Uzeri(this int sayi, int sayi2)
+    {
+        return (int)Math.Pow(sayi, sayi2);
+    }
+}
